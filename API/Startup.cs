06263ed6585b52f6a0191a27e590f0e3140120a8
vijaykumarrpai <vijaykumarrpai@gmail.com>
2020-09-03 20:01:20 +0530

    Setting up react

--- API/Startup.cs
+++ API/Startup.cs
@@ -28,8 +28,12 @@ namespace API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<DataContext>(opt => {
-                opt.UseSqlite(Configuration.GetConnectionString
-                ("DefaultConnection"));
+                opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
+            });
+            services.AddCors(opt => {
+                opt.AddPolicy("CorsPolicy", policy => {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
             });
             services.AddControllers();
         }
@@ -48,6 +52,8 @@ namespace API
 
             app.UseAuthorization();
 
+            app.UseCors("CorsPolicy");
+            
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
