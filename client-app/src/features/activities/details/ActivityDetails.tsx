bd662dd51c572b2f2459b1c712bc3f98839b2098
Vijaykumar R Pai <vijaykumarrpai@gmail.com>
2020-09-16 12:24:06 +0530

    Error handling and validation

--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -13,15 +13,18 @@ interface DetailsParams {
     id: string
 }
 
-const ActivityDetails: React.FC<RouteComponentProps<DetailsParams>> = ({ match }) => {
+const ActivityDetails: React.FC<RouteComponentProps<DetailsParams>> = ({ match, history }) => {
     const activityStore = useContext(ActivityStore)
     const { activity, loadActivity, loadingInitial } = activityStore;
 
     useEffect(() => {
-        loadActivity(match.params.id)
-    }, [loadActivity, match.params.id]);
+        loadActivity(match.params.id);
+    }, [loadActivity, match.params.id, history]);
 
-    if (loadingInitial || !activity) return <LoadingComponent content='Loading activity...' />
+    if (loadingInitial) return <LoadingComponent content='Loading activity...' />;
+
+    if (!activity)
+        return <h2>Activity not found</h2>
 
     return (
         <Grid>
